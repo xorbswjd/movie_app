8179ab4a221dc554477435dca91a54c834fb0c8e
xorbswjd <xorbswjd123@naver.com>
2020-03-07 14:05:19 +0900

    Updates

---
+++ precache-manifest.90ad44191ca7499abcfb52fe38ef055e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3d52299316f576e0a18548d5c28ded",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f6d7043faaceef2ac62c",
+    "url": "/movie_app/static/css/main.94552e6a.chunk.css"
+  },
+  {
+    "revision": "99fa8669b75eac161b6c",
+    "url": "/movie_app/static/js/2.f87ecc07.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.f87ecc07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d7043faaceef2ac62c",
+    "url": "/movie_app/static/js/main.4daf7201.chunk.js"
+  },
+  {
+    "revision": "c145c45984fe2e41280f",
+    "url": "/movie_app/static/js/runtime-main.ffbc15b4.js"
+  }
+]);
