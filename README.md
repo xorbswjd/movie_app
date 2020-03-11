75a7e1e61bf61868cda6146eacf25bfd38753d56
xorbswjd <xorbswjd123@naver.com>
2020-03-11 17:17:21 +0900

    second

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #Movie App
 
-React JS Fundamentals (2020)
+React JS Fundamentals Course (2020)
---
+++ src/components/Movie.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Porptype from 'prop-types';
+import "./Movie.css";
+
+function Movie({id, year, title, summary, poster, genres}) {
+return (
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+          }
+        }}
+      >
+        <img src = { poster } alt = { title } title = { title }/>
+            <div className = "movie_data">
+                <h3 className = "movie_title">{ title }</h3>
+                <h5 className = "movie_year">{ year }</h5>
+                <ul className = "genres">
+                    { genres.map((genre, index) => (
+                        <li key = { index } className = "genres_genre">
+                            { genre }
+                        </li>
+                    ))}
+                </ul> 
+                <p className = "movie_summary">{ summary.slice(0, 180) }...</p>
+            </div>
+        </Link>
+    </div>
+    );
+}
+
+Movie.prototype = {
+    id: Porptype.number.isRequired,
+    year: Porptype.number.isRequired,
+    title: Porptype.string.isRequired,
+    summary: Porptype.string.isRequired,
+    poster: Porptype.string.isRequired,
+    genres: Porptype.arrayOf(Porptype.string).isRequired
+}
+
+export default Movie;
